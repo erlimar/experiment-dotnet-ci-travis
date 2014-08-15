197db116ab15645d54c1ac47d40a67c22bc77d13
Erlimar Silva Campos <erlimar@gmail.com>
2014-08-15 14:46:01 -0300

    Testes falhos inclusos

---
+++ src/E5R.Experiment.CI.Travis.Tests/Feature.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace E5R.Experiment.CI.Travis.Tests
+{
+    using System;
+    using Target = E5R.Experiment.CI.Travis;
+
+    public class Feature
+    {
+        [Fact]
+        public void ValidatesNameIsNotNull()
+        {
+            Assert.Throws<NotImplementedException>(() =>
+            {
+                new Target.Feature(null);
+            });
+        }
+
+        [Fact]
+        public void ValidatesNameIsNotEmpty()
+        {
+            Assert.Throws<NotImplementedException>(() =>
+            {
+                new Target.Feature("");
+            });
+        }
+
+        [Fact]
+        public void StoresTheNameInLowercase()
+        {
+            var subject = new Target.Feature("My UPPERcase Feature");
+            Assert.Equal("my uppercase feature", subject.Name);
+        }
+    }
+}
---
+++ src/E5R.Experiment.CI.Travis/Feature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E5R.Experiment.CI.Travis
+{
+    public class Feature
+    {
+        public string Name { get; private set; }
+
+        public Feature(string name)
+        {
+
+        }
+    }
+}
